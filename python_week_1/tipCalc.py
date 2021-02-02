61290d6a01bc1682eb4e6e65a214d00826a12935
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-02 10:54:24 -0600

    tip calculator

---
+++ python_week_1/tipCalc.py
@@ -0,0 +1,18 @@
+amount = int(input('Enter Bill Amount: '))
+tip = input('Enter tip percentage: a = 10%, b = 15%, c = 20%, d = $25:  ')
+
+def calculateTip():
+    if tip == 'a':
+        result = .10 * amount
+    elif tip == 'b':
+        result = .15 * amount
+    elif tip == 'c':
+        result = .20 * amount
+    elif tip == 'd':
+        result = .25 * amount
+    
+    total_amount = amount + result
+    tip_amount = result
+    print(f'The total amount: ${total_amount} and the amount you tipped is ${tip_amount}')
+
+calculateTip()
