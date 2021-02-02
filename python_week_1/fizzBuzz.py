8d010c1e4f9f32546250b8b6789b69daf364f62a
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-02 11:12:19 -0600

    fizzbuzz change

--- python_week_1/fizzBuzz.py
+++ python_week_1/fizzBuzz.py
@@ -1,12 +1,12 @@
 number = int(input('Please enter a number: '))
 
 def fizzBuzz(number):
-    if number % 3 == 0:
+    if number % 3 == 0 & number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
         print('Fizz')
     elif number % 5 == 0:
         print('Buzz')
-    elif number % 3 == 0 & number % 5 == 0:
-        print('FizzBuzz')
     else:
         print(f'Number does not fit requirements: {number}')
 
