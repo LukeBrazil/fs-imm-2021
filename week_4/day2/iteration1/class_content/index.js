591b07168ec78f2dd876a0f12d25d6a66bdf2c15
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-23 15:54:00 -0600

    day2

--- week_4/day2/iteration1/class_content/index.js
+++ week_4/day2/iteration1/class_content/index.js
@@ -1,21 +1,40 @@
 const submitButton = document.getElementById('submitButton')
 const firstName = document.getElementById('fName')
-const lastName = document.getElementById('lName')
-const fullName = document.getElementById('fullName')
+const task = document.getElementById('task')
 
+const pendingTasks = document.getElementById('pendingTasks')
+const completedTasks = document.getElementById('completedTasks')
 const redButton = document.getElementById('red')
 const blueButton = document.getElementById('blue')
+const removeButton = document.getElementById('removeButton')
+
+
 
 submitButton.addEventListener('click', function() {
-    let fName = firstName.value
-    let lName = lastName.value
-    fullName.innerHTML = fName + ' ' + lName
+    let taskItem = task.value
+    task.value = ''
+    let liItem = document.createElement('li')
+    let checkBox = document.createElement('input')
+    checkBox.setAttribute('type', 'checkbox')
+    checkBox.setAttribute('id', 'checkBox')
+    checkBox.addEventListener('change', function() {
+        if (this.checked) {
+            completedTasks.appendChild(this.parentElement)
+        } else {
+            pendingTasks.appendChild(this.parentElement)
+        }
+    })
+    let removeButton = document.createElement('button')
+    removeButton.innerText = 'Remove'
+    removeButton.setAttribute('id', 'removeButton')
+    removeButton.addEventListener('click', function() {
+        this.parentElement.remove()
+    })
+    liItem.innerHTML = taskItem
+    liItem.appendChild(checkBox)
+    liItem.appendChild(removeButton)
+    pendingTasks.appendChild(liItem)
 })
 
-redButton.addEventListener('click', function() {
-    document.body.style.background = 'red'
-})
 
-blueButton.addEventListener('click', function() {
-    document.body.style.background = 'blue'
-})
+
