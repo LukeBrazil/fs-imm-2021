b5f939844af72831bdf7730e01bca5a5cf264fff
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-25 10:59:03 -0600

    day4

---
+++ week_4/day4/stocks/index.js
@@ -0,0 +1,27 @@
+const quoteSelect = document.getElementById("quoteSelect");
+const contentDiv = document.getElementById("content");
+const stockText = document.getElementById('stockText')
+const btnStock = document.getElementById('btnStock')
+
+btnStock.addEventListener('click', function() {
+    let stockSymbol = stockText.value
+    getStock(stockSymbol)
+    let intervalId = window.setInterval(() => {
+        getStock(stockSymbol)
+    }, 5000)
+})
+
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getStock(value) {
+  contentDiv.innerHTML = "";
+  let stock = quotes[value];
+  let price = getRandomInt(0, 500);
+  let message = `${stock.name}: ${price}`;
+  console.log(message);
+  contentDiv.innerHTML += message;
+}
