11be73ee8f8d15c9b664280f32ddfa524b39c706
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-25 11:21:26 -0600

    day4

--- week_4/day4/stocks/index.js
+++ week_4/day4/stocks/index.js
@@ -1,17 +1,21 @@
 const quoteSelect = document.getElementById("quoteSelect");
 const contentDiv = document.getElementById("content");
-const stockText = document.getElementById('stockText')
-const btnStock = document.getElementById('btnStock')
+const stockText = document.getElementById('stockText');
+const btnStock = document.getElementById('btnStock');
 
 btnStock.addEventListener('click', function() {
     let stockSymbol = stockText.value
-    getStock(stockSymbol)
-    let intervalId = window.setInterval(() => {
+    let checkedSymbol = checkSymbol(stockSymbol)
+    if (checkedSymbol === true) {
         getStock(stockSymbol)
-    }, 5000)
+        let intervalId = window.setInterval(() => {
+            getStock(stockSymbol)
+        }, 5000)
+    } else {
+        alert('INVALID SYMBOL')
+    }
+    
 })
-
-
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,4 +28,13 @@ function getStock(value) {
   let message = `${stock.name}: ${price}`;
   console.log(message);
   contentDiv.innerHTML += message;
+}
+
+function checkSymbol(symbol) {
+    if (quotes[symbol] === undefined) {
+        contentDiv.innerHTML = `${symbol} not available`
+        return false
+    } else {
+        return true
+    }
 }
