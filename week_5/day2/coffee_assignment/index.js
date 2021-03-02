715bb8c42d640ad158b06cc0387bb77d02044459
LukeBrazil <Luke.Brazil13@icloud.com>
2021-03-02 12:42:02 -0600

    day2

---
+++ week_5/day2/coffee_assignment/index.js
@@ -0,0 +1,107 @@
+const coffeeEmailInput = document.getElementById("coffeeEmail");
+const coffeeTypeInput = document.getElementById("coffeeType");
+const coffeeSizeInput = document.getElementById("coffeeSize");
+const coffeePriceInput = document.getElementById("coffeePrice");
+const newCoffeeOrderBtn = document.getElementById("newCoffeeOrderBtn");
+const displayOrders = document.getElementById('displayOrders')
+const getOrderByEmailDiv = document.getElementById('getOrderByEmail')
+const orderEmailInput = document.getElementById('orderEmail')
+const orderByEmailBtn = document.getElementById('orderByEmailBtn')
+const deleteOrderByEmailBtn = document.getElementById('deleteOrderByEmailBtn')
+
+const ordersUrl = "https://troubled-peaceful-hell.glitch.me/orders";
+
+const newCoffeOrder = () => {
+    const coffeeEmail = coffeeEmailInput.value;
+    const coffeeType = coffeeTypeInput.value;
+    const coffeeSize = coffeeSizeInput.value;
+    const coffeePrice = coffeePriceInput.value;
+    let request = new XMLHttpRequest
+
+    const requestParams = {
+        email: coffeeEmail,
+        type: coffeeType,
+        size: coffeeSize,
+        price: parseInt(coffeePrice)
+    }
+
+    request.open('POST', ordersUrl)
+    request.setRequestHeader('Content-Type', 'application/json')
+    request.send(JSON.stringify(requestParams))
+}
+
+function showOrdersByEmail() {
+    let request = new XMLHttpRequest();
+    const orderEmail = orderEmailInput.value
+
+  request.onload = function () {
+    let response = JSON.parse(this.responseText)
+    let order = `
+    <div class='orderItem'>
+        <ul>
+            <li>${response.email}</li>
+            <li>${response.type}</li>
+            <li>${response.size}</li>
+            <li>${response.price}</li>
+        </ul>
+    </div>
+    `
+    displayOrders.innerHTML = order
+  };
+
+  request.open("GET", `https://troubled-peaceful-hell.glitch.me/orders/${orderEmail}`);
+  request.send();
+}
+
+function deleteOrderByEmail() {
+    let request = new XMLHttpRequest();
+    const orderEmail = orderEmailInput.value
+
+  request.open("DELETE", `https://troubled-peaceful-hell.glitch.me/orders/${orderEmail}`);
+  request.send();
+}
+
+function showOrders() {
+    let request = new XMLHttpRequest();
+  
+
+  request.onload = function () {
+    let response = JSON.parse(this.responseText)
+    let orders = response.map((order) => {
+      return `
+            <div class='orderItem'>
+                <ul>
+                    <li>${order.email}</li>
+                    <li>${order.type}</li>
+                    <li>${order.size}</li>
+                    <li>${order.price}</li>
+                </ul>
+            </div>
+            `;
+    });
+    displayOrders.innerHTML = orders.join('')
+  };
+
+  request.open("GET", ordersUrl);
+  request.send();
+}
+
+newCoffeeOrderBtn.addEventListener('click', function() {
+    newCoffeOrder()
+})
+
+
+orderByEmailBtn.addEventListener('click', function() {
+    showOrdersByEmail()
+})
+
+deleteOrderByEmailBtn.addEventListener('click', function() {
+    deleteOrderByEmail()
+})
+
+
+window.addEventListener("load", function () {
+  showOrders()
+});
+
+
