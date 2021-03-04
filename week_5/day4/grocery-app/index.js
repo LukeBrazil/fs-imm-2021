dea4cbdb786c7cfbc30fb6cd940d1d7d5026e0b0
LukeBrazil <Luke.Brazil13@icloud.com>
2021-03-04 14:25:16 -0600

    day4

--- week_5/day4/grocery-app/index.js
+++ week_5/day4/grocery-app/index.js
@@ -1,93 +1,101 @@
-console.log(db)
-
-const groceryStoreText = document.getElementById('groceryStoreText')
-const addGroceryStoreBtn = document.getElementById('addGroceryStoreBtn')
-const showStoresUL = document.getElementById('showStoresUL')
-const groceryStoreAddressText = document.getElementById('groceryStoreAddressText')
-
-const storesRef = db.collection('stores');
+const groceryStoreText = document.getElementById("groceryStoreText");
+const addGroceryStoreBtn = document.getElementById("addGroceryStoreBtn");
+const showStoresUL = document.getElementById("showStoresUL");
+const groceryStoreAddressText = document.getElementById("groceryStoreAddressText");
+const storesRef = db.collection("stores");
 
 function getAllStores() {
-    
-    db.collection('stores')
-        .get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                let data = doc.data()
-                console.log(doc.id)
-                let storeDoc = `<li>${data.name}</li>
+  storesRef
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = doc.data();
+        generateStores(data, doc);
+        showItems(doc.id);
+      });
+    });
+}
+
+function createStore() {
+  const storeName = groceryStoreText.value;
+  const storeAddress = groceryStoreAddressText.value;
+
+  storesRef
+    .add({
+      name: storeName,
+      address: storeAddress,
+    })
+    .then(() => {
+      window.location.reload();
+    });
+}
+
+function generateStores(data, doc) {
+  let storeDoc = `<li>${data.name}</li>
                                 <li>${data.address}</li>
                                 <button id='deleteStoreBtn' onClick=deleteStore('${doc.id}')>Delete Store</button>
-                                <input type="text" id="addItem" placeholder="Add Item">
+                                <input type="text" id="addItem${doc.id}" placeholder="Add Item">
                                 <button id='addItemBtn' onClick=addItem('${doc.id}')>Add Item</button>
                                 <ul id='storeItemsUL'>Items:
 
                                 </ul>
-                                `
-                showStoresUL.insertAdjacentHTML('afterbegin', storeDoc)
-                showItems(doc.id)
-            })
-            
-        })
-        
+                                `;
+  showStoresUL.insertAdjacentHTML("afterbegin", storeDoc);
 }
 
 function deleteStore(id) {
-    storesRef.doc(id).delete().then(() => {
-        window.location.reload()
-    })
-}
-
-function addItem(id) {
-    const addItemText = document.getElementById('addItem')
-    let item = addItemText.value
-    storesRef.doc(id).collection('items').add({
-        name: item
-    }).then(() => {
-        window.location.reload()
-    })
+  storesRef
+    .doc(id)
+    .delete()
+    .then(() => {
+      window.location.reload();
+    });
 }
 
 function showItems(storeID) {
-    const storeItemsUL = document.getElementById('storeItemsUL')
-    storesRef.doc(storeID).collection('items')
-        .get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                console.log('STORE ID:', storeID)
-                let data = doc.data()
-                console.log(data.name)
-                let storeItem = `
+  const storeItemsUL = document.getElementById("storeItemsUL");
+  storesRef
+    .doc(storeID)
+    .collection("items")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = doc.data();
+        let storeItem = `
                     <li>${data.name}</li>
                     <button onClick="deleteItem('${storeID}', '${doc.id}')">Delete Item</button>
-                `
-                storeItemsUL.insertAdjacentHTML('beforeend', storeItem)
-            })
-           
-        })
+                `;
+        storeItemsUL.insertAdjacentHTML("beforeend", storeItem);
+      });
+    });
 }
+
+function addItem(id) {
+  const addItemText = document.getElementById(`addItem${id}`);
+  let item = addItemText.value;
+  storesRef
+    .doc(id)
+    .collection("items")
+    .add({
+      name: item,
+    })
+    .then(() => {
+      window.location.reload();
+    });
+}
+
 
 function deleteItem(storeID, docID) {
-    storesRef.doc(storeID)
-        .collection('items')
-        .doc(docID)
-        .delete()
-        .then(() => {window.location.reload()})
+  storesRef
+    .doc(storeID)
+    .collection("items")
+    .doc(docID)
+    .delete()
+    .then(() => {
+      window.location.reload();
+    });
 }
 
-addGroceryStoreBtn.addEventListener('click', function() {
-    const storeName = groceryStoreText.value
-    const storeAddress = groceryStoreAddressText.value
+addGroceryStoreBtn.addEventListener("click", function () {createStore()});
 
-    storesRef.add({
-        name: storeName,
-        address: storeAddress
-    }).then(() => {
-        window.location.reload()
-    })
-    
-})
-
-window.addEventListener('load', function() {
-    getAllStores()
-})
+window.addEventListener("load", function () {getAllStores()});
