2ccada33947f1894e1a3dc3d44f1bd2b9d8728e4
LukeBrazil <Luke.Brazil13@icloud.com>
2021-03-16 11:19:59 -0500

    day2

--- week7/day2/app.js
+++ week7/day2/app.js
@@ -9,13 +9,36 @@ app.set('views', './views')
 
 app.set('view engine', 'mustache')
 
+app.use(express.urlencoded())
+
 let tasks = [
     {title: 'Wash Car', priority: 'High'},
-    {title: 'Do Dishes', priority: 'High'}
+    {title: 'Do Dishes', priority: 'High'},
+    {title: 'Do Homework', priority: 'High'}
 ]
 
+app.get('/addTasks', (req, res) => {
+    res.render('addTasks')
+})
+
+app.post('/addTasks', (req, res) => {
+    const task = {title: req.body.title, priority: req.body.priority}
+    tasks.push(task)
+    res.redirect('/tasks')
+})
+
 app.get('/tasks', (req, res) => {
-    res.render('tasks', { allTasks: tasks })
+    res.render('tasks', { allTasks: tasks, totalTasks: tasks.length})
+})
+
+app.get('/add-customer', (req, res) => {
+    res.render('add-customer')
+})
+
+app.post('/add-customer', (req,res) => {
+    const firstName = req.body.firstName
+    const age = req.body.age
+    res.render('confirmation', {firstName: firstName, age: age})
 })
 
 app.get('/', (req,res) => {
