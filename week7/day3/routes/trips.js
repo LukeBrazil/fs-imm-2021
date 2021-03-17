be0cceb15757bf746737e44885c761345f254433
LukeBrazil <Luke.Brazil13@icloud.com>
2021-03-17 15:35:28 -0500

    day3

--- week7/day3/routes/trips.js
+++ week7/day3/routes/trips.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const app = express()
 
-let trips = []
+
 
 router.get('/', (req, res) => {
     res.render('trips', {allTrips: trips})
+})
+
+router.get('/:tripName', (req, res) => {
+    const tripName = req.params.tripName
+    res.render('tripName', {tripName: tripName})
 })
 
 router.post('/trips-post', (req, res) => {
@@ -19,6 +23,7 @@ router.post('/trips-delete', (req, res) => {
     const id = req.body.id
     let trip = trips.findIndex(trip => trip.id == id)
     trips.splice(trip, 1)
+    trips = trips.filter((trip) => trip.id != id)
     res.redirect('/trips')
 })
 
